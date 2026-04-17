6fd974ef5ab33d0041078025990925102da4c203
Antonio Doumit <83137353+AntDev112@users.noreply.github.com>
2026-04-17 11:28:24 +0100

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    private int n;
+    private int k;
+    private List<List<Integer>> result;
+    private List<Integer> curr;
+    public List<List<Integer>> combine(int n, int k) {
+        this.result = new ArrayList<>();
+        this.curr = new ArrayList<>();
+        this.n = n;
+        this.k = k;
+        backtrack(1);
+        return result;
+    }
+
+    public void backtrack(int i) {
+
+        if(curr.size() == k) {
+            result.add(new ArrayList<>(curr));
+            return;
+        }
+
+        if(i > n || curr.size() > k) return;
+
+        curr.add(i);
+        backtrack(i+1);
+        curr.remove(curr.size()-1);
+        backtrack(i+1);
+    }
+}
